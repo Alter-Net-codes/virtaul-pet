64408a5ad3b4d59279930428e8677b85d60189d2
webbrowser11 <terpstragraham@gmail.com>
2024-08-01 17:05:49 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+status = "happy"
+
 print("welcome to the virtaul pet game")
 print("keep your pet happy by feeding it...")
 print("play with it...")
 print("make it rest...")
-print("and but it toys")
+print("and buy it toys")
 print("enjoy this simple command line game by Alter Net codes!")
 input("Press Enter to continue...")
-import os
-# Clearing the Screen
-os.system('cls')
+for i in range(20):
+    print(" ")
+print("looks like your pet is", status)
+task = input("what would you like to do? ")
