0c37f24680c49c6d5168330f0922434088bd9795
webbrowser11 <terpstragraham@gmail.com>
2024-08-01 17:11:15 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,5 +9,12 @@ print("enjoy this simple command line game by Alter Net codes!")
 input("Press Enter to continue...")
 for i in range(20):
     print(" ")
+while True
 print("looks like your pet is", status)
+print(you can FEED, PLAY, REST, BUY TOYS, EXIT)
 task = input("what would you like to do? ")
+ if task == "play":
+     print("you go to the park to play with your dog!")
+     status = "excited"
+     print("your dog is", status,"to be at the park")
+     print("")
