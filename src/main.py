ba5543afca29137373774f2f3e81969f2e093525
webbrowser11 <terpstragraham@gmail.com>
2024-08-01 17:00:00 -0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+print("welcome to the virtaul pet game")
+print("keep your pet happy by feeding it...")
+print("play with it...")
+print("make it rest...")
+print("and but it toys")
+print("enjoy this simple command line game by Alter Net codes!")
+input("Press Enter to continue...")
+import os
+# Clearing the Screen
+os.system('cls')
